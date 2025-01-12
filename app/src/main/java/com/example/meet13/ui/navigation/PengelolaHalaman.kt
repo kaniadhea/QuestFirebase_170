c30fe5fe085f58da232e7e99a98e21d66a41abad
kaniadhea <kaniadhea7@gmail.com>
2025-01-12 13:33:46 +0700

    membuat fun PengelolaHalaman

---
+++ app/src/main/java/com/example/meet13/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.meet13.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.meet13.ui.view.HomeScreen
+import com.example.meet13.ui.view.InsertMhsView
+
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
