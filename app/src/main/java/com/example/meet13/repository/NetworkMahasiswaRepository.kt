8b307ecb5e02d4abad7ab2fd4df76bbdc2de6fc3
kaniadhea <kaniadhea7@gmail.com>
2025-01-12 09:54:20 +0700

    insertmhs agar bisa tambh data mahasiswa

--- app/src/main/java/com/example/meet13/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/meet13/repository/NetworkMahasiswaRepository.kt
@@ -6,10 +6,22 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkMahasiswaRepository (
     private val firestore: FirebaseFirestore
 ): MahasiswaRepository{
+
+    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+        try {
+            firestore.collection("Mahasiswa"). add(mahasiswa).await()
+        } catch (e: Exception){
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
+    }
+
+
+
     override suspend fun getMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
 
         val mhsCollection = firestore.collection("Mahasiswa")
