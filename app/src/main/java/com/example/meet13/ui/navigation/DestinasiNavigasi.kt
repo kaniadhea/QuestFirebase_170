a3d1bb936dfd44180c54f7777c7b4143a9db93a0
kaniadhea <kaniadhea7@gmail.com>
2025-01-12 13:07:23 +0700

    yang digunakan untuk mendefinisikan destinasi dalam sistem navigasi

---
+++ app/src/main/java/com/example/meet13/ui/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,16 @@
+package com.example.meet13.ui.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val title: String
+}
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route: String = "home"
+    override val title: String ="Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi{
+    override val route: String = "insert"
+    override val title: String = "Insert"
+}
